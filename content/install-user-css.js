becc0d7dba3f75c4062ec8cd61d5469de1681610
eight <eight04@gmail.com>
2017-08-06 00:49:25 +0800

    Add: install styles from *.user.css file

---
+++ content/install-user-css.js
@@ -0,0 +1,31 @@
+/* globals userStyle2json */
+
+'use strict';
+
+function fetchText(url) {
+  return new Promise((resolve, reject) => {
+    // you can't use fetch in Chrome under 'file:' protocol
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', () => resolve(xhr.responseText));
+    xhr.addEventListener('error', () => reject(xhr));
+    xhr.send();
+  });
+}
+
+function install() {
+  fetchText(location.href).then(source => {
+    const request = {
+      method: 'saveStyle',
+      url: location.href,
+      updateUrl: location.href
+    };
+    Object.assign(request, userStyle2json(source));
+    chrome.runtime.sendMessage(request);
+  }).catch(console.log);
+}
+
+// FIXME: i18n
+if (confirm('Do you want to install this style into stylus?')) {
+  install();
+}
---
+++ js/userstyle.js
@@ -0,0 +1,117 @@
+'use strict';
+
+function wildcard2regexp(text) {
+  return text.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&').replace(/\\\*/g, '.*');
+}
+
+
+function userStyle2json(source) {
+  const style = buildStyle(source);
+
+  // convert @include rules to stylish
+  // maybe we should parse match patterns in the future
+  // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Match_patterns
+  for (const section of style.sections) {
+    for (const include of section.includes) {
+      let m;
+      if (include === '*') {
+        // match all
+        continue;
+      } else if (include.startsWith('/') && include.endsWith('/')) {
+        // regexp
+        if (!section.regexps) {
+          section.regexps = [];
+        }
+        section.regexps.push(include.slice(1, -1));
+      } else if (!include.includes('*')) {
+        // url
+        if (!section.urls) {
+          section.urls = [];
+        }
+        section.urls.push(include);
+      } else if ((m = include.match(/^\*:\/\/(?:\*\.)?([^/]+)\/\*$/))) {
+        // domain. Compatible with match patterns
+        // e.g. *://*.mozilla.org/*
+        if (!section.domains) {
+          section.domains = [];
+        }
+        section.domains.push(m[1]);
+      } else if ((m = include.match(/^[^*]+\*$/))) {
+        // prefixes
+        if (!section.urlPrefixes) {
+          section.urlPrefixes = [];
+        }
+        section.urlPrefixes.push(include.slice(0, -1));
+      } else {
+        // compile wildcard to regexps
+        if (!section.regexps) {
+          section.regexps = [];
+        }
+        section.regexps.push(wildcard2regexp(include));
+      }
+    }
+  }
+
+  return style;
+
+  function buildStyle(source) {
+    const commentRe = /\/\*[\s\S]*?\*\//g;
+    const metaRe = /==userstyle==[\s\S]*?==\/userstyle==/i;
+
+    const style = {
+      name: null,
+      isUserStyle: true,
+      source: source,
+      enabled: true,
+      sections: []
+    };
+    // iterate through each comment
+    let m;
+    while ((m = commentRe.exec(source))) {
+      const commentSource = source.slice(m.index, m.index + m[0].length);
+
+      let n = commentSource.match(metaRe);
+      if (!n) {
+        continue;
+      }
+
+      const section = {
+        commentStart: m.index,
+        commentEnd: m.index + m[0].length,
+        code: null, // calculate this later
+        includes: [],
+        excludes: []
+      };
+
+      const metaSource = n[0];
+
+      // FIXME: finish all metas
+      if ((n = metaSource.match(/@name\s+(.+)/))) {
+        style.name = n[1];
+      }
+      if ((n = metaSource.match(/@namespace\s+(\S+)/))) {
+        style.namespace = n[1];
+      }
+      let r = /@include\s+(\S+)/g;
+      while ((n = r.exec(metaSource))) {
+        section.includes.push(n[1]);
+      }
+      r = /@exclude\s+(\S+)/g;
+      while ((n = r.exec(metaSource))) {
+        section.excludes.push(n[1]);
+      }
+
+      style.sections.push(section);
+    }
+
+    // split source into `section.code`
+    for (let i = 0, len = style.sections.length; i < len; i++) {
+      style.sections[i].code = source.slice(
+        i === 0 ? 0 : style.sections[i].commentStart,
+        style.sections[i + 1] && style.sections[i + 1].commentStart
+      );
+    }
+
+    return style;
+  }
+}
